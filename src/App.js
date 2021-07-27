578cff04185e9e3efe09df1e1c1600c7390d8af7
River <moonquakes7@gmail.com>
2021-07-28 01:35:01 +0800

    演示生命周期 shouldComponentUpdate()

--- src/App.js
+++ src/App.js
@@ -2,48 +2,39 @@ import React from 'react';
 
 class App extends React.Component {
     constructor(props) {
-        super();
-        this.state = {x:1}
+        super(props);
+        this.state = {
+            n: 1
+        }
     }
+
     onClick = () => {
-        this.setState({
-                x: this.state.x + 1  // 1
-            }, ()=>{
-                this.setState({
-                    x: this.state.x + 1  // 1
-                })
-            }
-        )
+        this.setState(state=>({
+            n: state.n + 1
+        }));
+        this.setState(state=>({
+            n: state.n - 1
+        }))
     }
 
-    onClick2 = () => {
-        this.setState((state) => ({x: state.x + 1}))
-        this.setState((state) => ({x: state.x + 1}))
+    shouldComponentUpdate(newProps,newState) {
+        if(newState.n === this.state.n){
+            return false
+        } else {
+            return true
+        }
     }
 
     render() {
+        console.log('render 了一次');
         return (
-            <div className="App">
+            <div>
                 App
-                <button onClick={this.onClick2}>+1</button>
-                <B name={this.state.x}/>
+                <div>
+                    {this.state.n}
+                    <button onClick={this.onClick}>+1</button>
+                </div>
             </div>
-        )
-    }
-}
-
-class B extends React.Component {
-    UNSAFE_componentWillReceiveProps(nextProps, nextContext) {
-        console.log('旧的 props')
-        console.log(this.props);
-        console.log('props 变化了');
-        console.log('新的 props');
-        console.log(nextProps);
-    }
-
-    render() {
-        return (
-            <div>{this.props.name}</div>
         )
     }
 }
